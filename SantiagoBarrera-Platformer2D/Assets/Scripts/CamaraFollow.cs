531de8c40984e6189565cb067dfda0d17d5b57d2
santiagos7 <santiagodsbg@gmail.com>
2018-10-27 11:01:36 -0500

    no message

---
+++ SantiagoBarrera-Platformer2D/Assets/Scripts/CamaraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraFollow : MonoBehaviour {
+
+
+
+        public Transform mario;
+
+        void LateUpdate()
+        {
+            this.transform.localPosition = new Vector3(mario.transform.localPosition.x, 0, -10);
+        }
+
+    }
---
+++ SantiagoBarrera-Platformer2D/Assets/Scripts/MarioController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioController : MonoBehaviour {
+
+    public Animator animatorMario;
+    bool isRunning = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Referenccia componenete animator
+        animatorMario = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Por default,, suponemos que el ususario no esta presionando
+        // las flechas
+        isRunning = false;
+
+
+        //Flecha izquierda
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            this.transform.Translate(Vector3.left * 0.1f);
+            isRunning = true;
+            this.GetComponent<SpriteRenderer>().flipX = true;
+
+        }
+
+        // flecha derecha
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            this.transform.Translate(Vector3.right * 0.1f);
+            isRunning = true;
+            this.GetComponent<SpriteRenderer>().flipX = false;
+
+        }
+
+        //Movineod personaje si o no
+        animatorMario.SetBool("MarioIsRunning", isRunning);
+
+        //Saltar 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 7.0f, ForceMode2D.Impulse);
+            animatorMario.SetBool("MarioIsOnfloor", false);
+        }
+
+    } //Update end
+
+    //Cuando collider sonic toca otro collider
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        //Avisamos que el salto ha terminado
+        animatorMario.SetBool("MarioIsOnfloor", true);
+    }
+
+
+} //Script End
